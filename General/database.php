6f52260bb78948718577e561484c2622cbf66384
VarzeaNet <dev@varzeanet.com.br>
2024-05-20 16:38:26 -0300

    database update

--- General/database.php
+++ General/database.php
@@ -2,7 +2,7 @@
 
 define('HOST', '127.0.0.1');
 define('USUARIO', 'root');
-define('SENHA', 'sotapa157');
+define('SENHA', '');
 define('DB', 'abrigo');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die('Não foi possível conectar');
